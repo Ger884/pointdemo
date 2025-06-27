dce2537a2f09ad0a6ee33334c5655501e2706a03
Ger884 <thikkexrn@gmail.com>
2025-06-27 21:22:36 +0700

    DataBaseRt

---
+++ DataBase.php
@@ -0,0 +1,31 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "bottle_project";
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("เชื่อมต่อฐานข้อมูลล้มเหลว: " . $conn->connect_error);
+}
+
+$studentnumber = $_POST['studentnumber'];
+$graderoom = $_POST['graderoom'];
+$textdate = $_POST['textdate'];     
+$cap_code = (int)$_POST['cap_code'];
+
+$sql = "SELECT * FROM student_rt WHERE studentnumber = ? ORDER BY last_updated DESC LIMIT 1";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $studentnumber);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$sql = "INSERT INTO student_rt (studentnumber, graderoom, textdate, cap_code) 
+        VALUES (?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssi", $studentnumber, $graderoom, $textdate, $cap_code);
+$stmt->execute();
+
+header("Location: type.html");
+exit;
+?>
