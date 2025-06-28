abb78fc90392624e7b5e7817f9e0efd98c54bd96
Ger884 <thikkexrn@gmail.com>
2025-06-28 21:22:13 +0700

    ProcessHome.php

--- ProcessHome.php
+++ ProcessHome.php
@@ -11,8 +11,6 @@ if ($conn->connect_error) {
 
 $studentnumber = $_POST['studentnumber'];
 $graderoom = $_POST['graderoom'];
-$textdate = $_POST['textdate'];     
-$cap_code = (int)$_POST['cap_code'];
 
 $sql = "SELECT * FROM student_rt WHERE studentnumber = ? ORDER BY last_updated DESC LIMIT 1";
 $stmt = $conn->prepare($sql);
@@ -20,10 +18,10 @@ $stmt->bind_param("s", $studentnumber);
 $stmt->execute();
 $result = $stmt->get_result();
 
-$sql = "INSERT INTO student_rt (studentnumber, graderoom, textdate, cap_code) 
-        VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO student_rt (studentnumber, graderoom) 
+        VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sssi", $studentnumber, $graderoom, $textdate, $cap_code);
+$stmt->bind_param("ss", $studentnumber, $graderoom);
 $stmt->execute();
 
 header("Location: type.html");
